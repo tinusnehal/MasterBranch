8a52f28bc82437c77b4971b74eb406c432ae6cb6
tinusnehal <tinusnehal@gmail.com>
2022-08-13 15:10:16 +0530

    Master Branch Project added To Central Repository

---
+++ src/main/java/com/Bikkad/MasterBranchApplication.java
@@ -0,0 +1,13 @@
+package com.Bikkad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterBranchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Bikkad/MasterBranchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bikkad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterBranchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
