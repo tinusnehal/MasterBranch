c36ca88bb2c7b13e29527f6d8706d82fdf379169
tinusnehal <tinusnehal@gmail.com>
2022-08-13 15:53:03 +0530

    SA-1: Controller class added

---
+++ src/main/java/com/Bikkad/contoller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.Bikkad.contoller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	public void addEmployee() {
+		
+		
+	}
+}
