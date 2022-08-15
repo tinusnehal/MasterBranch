f942b06295ad9d50d1d2e8386a3b466a0db745ff
tinusnehal <tinusnehal@gmail.com>
2022-08-16 04:10:18 +0530

    SMS-01:Master1 Project Completed

---
+++ src/main/java/com/Bikkad/contoller/StudentController.java
@@ -0,0 +1,17 @@
+package com.Bikkad.contoller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class StudentController {
+
+	public List<String>getAllStudent(){
+		
+		
+		return null;
+		
+		
+		
+	}
+}
---
+++ src/main/java/com/Bikkad/service/StudentServiceImpl.java
@@ -0,0 +1,17 @@
+package com.Bikkad.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl {
+
+	public List<String>getAllStudent(){
+		
+		return null;
+		
+		
+		
+	}
+}
