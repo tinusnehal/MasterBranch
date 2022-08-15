54c0e22b5e89535d6fb0546fba83cdafdf93e33b
tinusnehal <tinusnehal@gmail.com>
2022-08-16 04:54:01 +0530

    git conflict

--- src/main/java/com/Bikkad/MasterBranchApplication.java
+++ src/main/java/com/Bikkad/MasterBranchApplication.java
@@ -8,6 +8,14 @@ public class MasterBranchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MasterBranchApplication.class, args);
+	
+	int a=25;
+	
+	
+	
 	}
-
+    
+	
+	
+	
 }
